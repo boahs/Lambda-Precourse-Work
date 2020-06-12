43eba4e8e72db892284b98a0702fe807ff6e5f99
boahs <39522778+boahs@users.noreply.github.com>
2020-06-12 08:59:02 -0400

    Create JS Assignment 6 Anatomy of A Function.js

---
+++ Introduction to JavaScript/JS Assignment 6 Anatomy of A Function.js
@@ -0,0 +1,12 @@
+// Exercise One:
+// Create a function named 'myFirstFunction', in it console log "this is my first function!"
+function myFirstFunction(first){
+    first = "this is my first function!";
+    console.log(first);
+  }
+// Exercise Two:
+// Create a function named 'mySecondFunction' in it console log 'this is my second function!'
+function mySecondFunction(second){
+  second = 'this is my second function!'
+  console.log(second);
+}
